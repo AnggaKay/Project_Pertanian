7178ac6775b1f5de273195fd4a3b6ba8ec40fd15
Angga Kay <anggakay22505@gmail.com>
2024-07-26 10:44:22 +0700

    tahap awal

---
+++ tes_dart_pertanian/bin/tes_dart_pertanian.dart
@@ -0,0 +1,5 @@
+import 'package:tes_dart_pertanian/tes_dart_pertanian.dart' as tes_dart_pertanian;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tes_dart_pertanian.calculate()}!');
+}
---
+++ tes_dart_pertanian/test/tes_dart_pertanian_test.dart
@@ -0,0 +1,8 @@
+import 'package:tes_dart_pertanian/tes_dart_pertanian.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
